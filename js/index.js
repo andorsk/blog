e1c07162ec146f79b9c79b4d410a3fe238b58b56
andorsk <andorsk@users.noreply.github.com>
2022-11-25 13:44:06 +0000

    deploy: 0512b440bfbb6d7238f001d73ca3ac80e98d52fe

---
+++ js/index.js
@@ -0,0 +1,48 @@
+document
+  .getElementById("toggle-buffer")
+  .addEventListener("click", toggleBuffer);
+
+function toggleBuffer() {
+  document.getElementById("buffer-selector").classList.toggle("hidden");
+}
+
+function sendMessage(msg) {
+  document.getElementById("message-buffer").innerText = msg;
+}
+
+document.addEventListener("keydown", (event) => {
+  console.log(event);
+  switch (event.code) {
+    case "KeyB":
+      toggleBuffer();
+      return;
+    case "KeyH":
+      toggleHelpModal();
+      return;
+    case "Slash":
+      toggleHelpModal();
+      return;
+    case "KeyP":
+      redirect("/posts");
+      return;
+    case "KeyT":
+      redirect("/tags");
+      return;
+    case "KeyM":
+      redirect("/");
+      return;
+  }
+});
+
+function toggleHelpModal() {
+  var x = document.getElementById("helpModal");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function redirect(path) {
+  window.location.href = path;
+}
